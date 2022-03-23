54446410d68d912cab625b086c1ce6f8850464df
Michael K <thkoon@connect.ust.hk>
2022-03-24 02:08:29 +0800

    logging and tg formatting

--- src/strategies/naive_arb.py
+++ src/strategies/naive_arb.py
@@ -5,7 +5,7 @@ from src.utils.logger import *
 logger = logging.getLogger("root")
 
 class NaiveArb:
-    def __init__(self, w3, token0, token1, exchange0, exchange1, exchange0_pair, exchange1_pair, executor, key):
+    def __init__(self, w3, token0, token1, exchange0, exchange1, exchange0_pair, exchange1_pair, executor, address, key):
         self.w3 = w3
         self.token0 = token0
         self.token1 = token1
@@ -14,15 +14,70 @@ class NaiveArb:
         self.exchange0_pair = exchange0_pair
         self.exchange1_pair = exchange1_pair
         self.executor = executor;
+        self.address = address
         self.key = key
+        self._update_balance()
         self._is_run = True
+
 
     def set_token0_size(self, size):
         self.token0_size = size * 10 ** self.token0[2]
+        size_log = f"Set token0 trading size to {size} {self.token0[0]}"
+        logging.info(size_log)
+        ping_telegram(size_log)
+
+
+    def _update_balance(self):
+        self.token0_bal = self.token0[3].functions.balanceOf(self.address).call() / 10 ** self.token0[2]
+        self.token1_bal = self.token1[3].functions.balanceOf(self.address).call() / 10 ** self.token1[2]
+
+
+    def ape(self, pair0, pair1, router0, router1, token0, token1, amount1):
+
+        token0_before = self.token0_bal
+        token1_before = self.token1_bal
+
+        aping_log = f"Aping: token0 - {token0_before}, token1 - {token1_before}"
+        logger.info(aping_log)
+        ping_telegram(aping_log)
+
+        tx = self.executor.functions.ape(
+                pair0, pair1, router0, router1, token0, token1, amount1
+            ).buildTransaction({
+                "from": self.address,
+                "gas": 2500000,
+                "nonce": self.w3.eth.get_transaction_count(self.address)
+            })
+        signed_tx = self.w3.eth.account.sign_transaction(tx, self.key)
+        h = self.w3.eth.send_raw_transaction(signed_tx.rawTransaction).hex()
+
+        self.w3.eth.wait_for_transaction_receipt(h)
+
+        self._update_balance()
+
+        token0_after = self.token0_bal
+        token1_after = self.token1_bal
+
+        
+        token0_diff = token0_after - token0_before
+        token0_pnl = (token0_diff / token0_before) if token0_diff else token0_diff
+
+        token1_diff = token1_after - token1_before
+        token1_pnl = (token1_diff / token31_before) if token1_diff else token1_diff
+
+        aped_log = \
+        f"Aped: token0 - {token0_after}, token1 - {token1_after}\n" + \
+        f"Token0: diff - {round(token0_diff, 3)}, pnl - {round(token0_pnl*100, 3)}%\n" + \
+        f"Token1: diff - {round(token1_diff, 3)}, pnl - {round(token1_pnl*100, 3)}%\n" + \
+        f"TX hash: {h}"
+
+        logger.info(aped_log)
+        ping_telegram(aped_log)
+
 
     def buy_token1_exchange0(self, exchange0_reserve, exchange1_reserve):
 
-        logger.info(f"Buy {self.token1[0]} @ {self.exchange0.name}, sell {self.token0[0]} @ {self.exchange1.name}")
+        logger.debug(f"Buy {self.token1[0]} @ {self.exchange0.name}, sell {self.token0[0]} @ {self.exchange1.name}")
 
         exchange0_token1_out = self.exchange0.router.functions.getAmountOut(self.token0_size, exchange0_reserve[0], exchange0_reserve[1]).call()
         exchange1_token0_out = self.exchange1.router.functions.getAmountOut(exchange0_token1_out, exchange1_reserve[1], exchange1_reserve[0]).call()
@@ -30,23 +85,27 @@ class NaiveArb:
         trade_1 = f"{self.token0_size / 10 ** self.token0[2]} {self.token0[0]} -> {exchange0_token1_out / 10 ** self.token1[2]} {self.token1[0]}"
         trade_2 = f"{exchange0_token1_out / 10 ** self.token1[2]} {self.token1[0]} -> {exchange1_token0_out / 10 ** self.token0[2]} {self.token0[0]}"
 
-        # logger.info(f"{trade_1} @ {self.exchange0.name}")
-        # logger.info(f"{trade_2} @ {self.exchange1.name}")
+        logger.debug(f"{trade_1} @ {self.exchange0.name}")
+        logger.debug(f"{trade_2} @ {self.exchange1.name}")
 
         pnl = exchange1_token0_out/self.token0_size - 1
-        # logger.info(f"PNL: {(exchange1_token0_out-self.token0_size) / 10 ** self.token0[2]} {self.token0[0]} ({round(pnl*100, 5)}%)")
+        logger.debug(f"PNL: {(exchange1_token0_out-self.token0_size) / 10 ** self.token0[2]} {self.token0[0]} ({round(pnl*100, 5)}%)")
 
         if pnl > 0:
-            ping_telegram(
-                "Trade Opportunity\n\n" + \
-                f"{trade_1} @ {self.exchange0.name}\n" + \
-                f"{trade_2} @ {self.exchange1.name}\n"+ \
-                f"{(exchange1_token0_out-self.token0_size) / 10 ** self.token0[2]} {self.token0[0]} ({round(pnl*100, 5)}%)"
+             self.ape(
+                self.exchange0_pair[0],
+                self.exchange1_pair[0],
+                self.exchange0.router_address,
+                self.exchange1.router_address,
+                self.token0[1],
+                self.token1[1],
+                exchange0_token1_out
             )
+
 
     def buy_token1_exchange1(self, exchange0_reserve, exchange1_reserve):
 
-        logger.info(f"Buy {self.token1[0]} @ {self.exchange1.name}, sell {self.token0[0]} @ {self.exchange0.name}")
+        logger.debug(f"Buy {self.token1[0]} @ {self.exchange1.name}, sell {self.token0[0]} @ {self.exchange0.name}")
 
         exchange1_token1_out = self.exchange1.router.functions.getAmountOut(self.token0_size, exchange1_reserve[0], exchange1_reserve[1]).call()
         exchange0_token0_out = self.exchange0.router.functions.getAmountOut(exchange1_token1_out, exchange0_reserve[1], exchange0_reserve[0]).call()
@@ -54,17 +113,14 @@ class NaiveArb:
         trade_1 = f"{self.token0_size / 10 ** self.token0[2]} {self.token0[0]} -> {exchange1_token1_out / 10 ** self.token1[2]} {self.token1[0]}"
         trade_2 = f"{exchange1_token1_out / 10 ** self.token1[2]} {self.token1[0]} -> {exchange0_token0_out / 10 ** self.token0[2]} {self.token0[0]}"
 
-        # logger.info(f"{trade_1} @ {self.exchange1.name}")
-        # logger.info(f"{trade_2} @ {self.exchange0.name}")
+        logger.debug(f"{trade_1} @ {self.exchange1.name}")
+        logger.debug(f"{trade_2} @ {self.exchange0.name}")
 
         pnl = exchange0_token0_out/self.token0_size - 1
-        logger.info(f"PNL: {(exchange0_token0_out-self.token0_size) / 10 ** self.token0[2]} {self.token0[0]} ({round(pnl*100, 5)}%)")
+        logger.debug(f"PNL: {(exchange0_token0_out-self.token0_size) / 10 ** self.token0[2]} {self.token0[0]} ({round(pnl*100, 5)}%)")
 
         if pnl > 0:
-            print(self.exchange1_pair[0])
-            print(self.exchange0_pair[0])
-            print(exchange0_token0_out)
-            tx = self.executor.functions.ape(
+            self.ape(
                 self.exchange1_pair[0],
                 self.exchange0_pair[0],
                 self.exchange1.router_address,
@@ -72,27 +128,14 @@ class NaiveArb:
                 self.token0[1],
                 self.token1[1],
                 exchange1_token1_out
-            ).buildTransaction({
-                "from": "0x058C6fC2cDf791e7B2516E931F112CC5556077Bc",
-                "gas": 2500000,
-                "nonce": self.w3.eth.get_transaction_count("0x058C6fC2cDf791e7B2516E931F112CC5556077Bc")
-            })
-            signed_tx = self.w3.eth.account.sign_transaction(tx, self.key)
-            h = self.w3.eth.send_raw_transaction(signed_tx.rawTransaction).hex()
-            print(h)
-            # ping_telegram(
-            #     "Trade Opportunity\n\n" + \
-            #     f"{trade_1} @ {self.exchange1.name}\n" + \
-            #     f"{trade_2} @ {self.exchange0.name}\n" + \
-            #     f"{(exchange0_token0_out-self.token0_size) / 10 ** self.token0[2]} {self.token0[0]} ({round(pnl*100, 5)}%)"
-            # )
+            )
+
 
 
     def run(self):
         while self._is_run:
             exchange0_reserve = self.exchange0_pair[1].functions.getReserves().call()
             exchange1_reserve = self.exchange1_pair[1].functions.getReserves().call()
-            print(exchange0_reserve)
-            print(exchange1_reserve)
+
             self.buy_token1_exchange0(exchange0_reserve, exchange1_reserve)
             self.buy_token1_exchange1(exchange0_reserve, exchange1_reserve)
--- trader.py
+++ trader.py
@@ -1,18 +1,20 @@
+import json
+import os
+
+from dotenv import load_dotenv
 from web3 import Web3
 from web3.middleware import geth_poa_middleware
 
-import os
-from dotenv import load_dotenv
 from src.constants import *
+from src.strategies.naive_arb import NaiveArb
 from src.utils.exchange_setup import Exchange
 from src.utils.logger import *
-from src.strategies.naive_arb import NaiveArb
-import json
 
 logger = setup_custom_logger("root")
 load_dotenv()
 
 RINKEBY_INFURA_URL = os.getenv("RINKEBY_INFURA_URL")
+PUBLIC_ADDRESS = os.getenv("PUBLIC_ADDRESS")
 PRIVATE_KEY = os.getenv("PRIVATE_KEY")
 
 def setup_provider(provider):
@@ -20,9 +22,21 @@ def setup_provider(provider):
     logger.info(f"Provider is ready: {w3.isConnected()}")
     return w3
 
-def setup_tokens(token0, token1):
+def setup_tokens(w3, token0, token1):
     a = token0 if token0 > token1 else token1
     b = token1 if token0 > token1 else token0
+
+    abi = json.load(open("./src/abi/ERC20.json"))["abi"]
+
+    a.append(w3.eth.contract(
+        address = a[1],
+        abi = abi
+    ))
+
+    b.append(w3.eth.contract(
+        address = b[1],
+        abi = abi
+    ))
 
     logger.info(f"Token0: {a[0]}")
     logger.info(f"Token1: {b[0]}")
@@ -35,32 +49,35 @@ def setup_dexes(w3):
     return Exchange0, Exchange1
 
 def main(a, b):
-    logger.info("Trader starting...")
-    # ping_telegram("Trader starting...")
+    starting_log = f"Trader starting... Bot @ {PUBLIC_ADDRESS}"
+    logger.info(starting_log)
+    ping_telegram(starting_log)
 
     w3 = setup_provider(RINKEBY_INFURA_URL)
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
-    # acc = w3.eth.account.privateKeyToAccount(PRIVATE_KEY)
-    # print(acc.address)
-    # print(w3.eth.default_account)
-    # print(w3.eth.accounts)
-    token0, token1 = setup_tokens(a, b)
+    token0, token1 = setup_tokens(w3, a, b)
 
     Exchange0, Exchange1 = setup_dexes(w3)
 
     Exchange0_pair = Exchange0.getPair(token0[1], token1[1])
     Exchange1_pair = Exchange1.getPair(token0[1], token1[1])
 
-    logger.info(f"{token0[0]}/{token1[0]} @ Uniswap {Exchange0_pair[0]}")
-    logger.info(f"{token0[0]}/{token1[0]} @ Sushiswap {Exchange1_pair[0]}")
+    exchange0_log = f"{token0[0]}/{token1[0]} @ Uniswap {Exchange0_pair[0]}"
+    exchange1_log = f"{token0[0]}/{token1[0]} @ Sushiswap {Exchange1_pair[0]}"
+
+    logger.info(exchange0_log)
+    logger.info(exchange1_log)
+
+    ping_telegram(exchange0_log)
+    ping_telegram(exchange1_log)
 
     # set target trade size
     executor = w3.eth.contract(
         address = RINKEBY_EXECUTOR,
         abi = json.load(open("./src/abi/Ape.json"))["abi"]
     )
-    strategy = NaiveArb(w3, token0, token1, Exchange0, Exchange1, Exchange0_pair, Exchange1_pair, executor, PRIVATE_KEY)
+    strategy = NaiveArb(w3, token0, token1, Exchange0, Exchange1, Exchange0_pair, Exchange1_pair, executor, PUBLIC_ADDRESS, PRIVATE_KEY)
     strategy.set_token0_size(1)
 
     strategy.run()
